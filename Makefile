a0b498dc05a9e590b1796063c795be5c43e2392a
honga789 <honganhnguyenpro655@gmail.com>
2024-04-03 22:25:47 +0700

    initial project version

---
+++ Makefile
@@ -0,0 +1,7 @@
+all:
+	@echo "nothing to do"
+
+.PHONY clean:
+clean:
+	rm -f ./src/*.exe
+	rm -f ./build/bin/*
---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+
+#define endl '/n'
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+
+int main()
+{
+    fast_cin();
+    freopen("D:/Documents/Code/TestST/GitTest/resources/input.txt", "r", stdin);
+    freopen("D:/Documents/Code/TestST/GitTest/resources/output.txt", "w", stdout);
+    int n;
+    cin >> n;
+    if(n % 2)
+        cout << n;
+    else
+        cout << n << "is even";
+    
+
+    return 0;
+}
