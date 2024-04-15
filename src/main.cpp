e19e2da1024bc2e12d9df04bea89b0e9f25346ff
honga789 <honganhnguyenpro655@gmail.com>
2024-04-15 23:37:41 +0700

    modified second time

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ int main()
     fast_cin();
     freopen("D:/Documents/Code/TestST/GitTest/resources/input.txt", "r", stdin);
     freopen("D:/Documents/Code/TestST/GitTest/resources/output.txt", "w", stdout);
-    cout << "modified first time";
+    cout << "modified second time";
     
     return 0;
 }
