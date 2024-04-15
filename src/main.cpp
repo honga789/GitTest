bec79809a23b2dc57d3b2bc17b7b9c1a73f47366
honga789 <honganhnguyenpro655@gmail.com>
2024-04-15 23:48:58 +0700

    add function tmp

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,11 @@ typedef vector<ll> vll;
 #define endl '/n'
 #define fast_cin() ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
 
+bool tmp()
+{
+    return false;
+}
+
 int main()
 {
     fast_cin();
